3c29531d79a8eb9212d630e8bdbcf43b886b727d
Nathaniel Hobbs <hobbs.nathaniel@gmail.com>
2016-08-19 00:26:21 +0800

    modified storing of plants in object by just using first name of plant for key to avoid strange characters

--- app.js
+++ app.js
@@ -38,7 +38,8 @@ app.post('/garden', function(req,res) {
 
   //because working with objects, don't need to check for existance of plant in garden
   //if new then this will add to garden, otherwise will update whatever properties changed
-  garden[plant.name] = plant;  
+  var key = lant.name.split(' ', 1)[0]; //first word of plant for key
+  garden[key] = plant;  
 
   res.send(garden);
 });
