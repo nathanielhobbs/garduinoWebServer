efc651d879309a1b3ce70f3c69bfe792f54af519
Nathaniel Hobbs <hobbs.nathaniel@gmail.com>
2016-08-18 13:57:57 +0800

    added jQuery for asyncrhonous updating of garden and refactored some code

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var app = express();
 var settings = require('./settings');
 var port = settings.port;
+var garden = {};
 
 
 // establish streams for RaspPi components
@@ -28,18 +29,25 @@ app.set('view engine', 'ejs');
 // pipe button presses to stdout
 // button.pipe(process.stdout);
 
-// API routes
-app.post('/plants', function(req,res) {
-  console.log('new plant:');
-  console.log(JSON.stringify(req.body))
+// body should be of form {"name": xxx,"moisture": xxx}
+app.post('/garden', function(req,res) {
+  console.log('received post from plant:');
+  console.log(JSON.stringify(req.body));
+  var plant = req.body;
+  plant.lastUpdate = new Date();
+
+  //because working with objects, don't need to check for existance of plant in garden
+  //if new then this will add to garden, otherwise will update whatever properties changed
+  garden[plant.name] = plant;  
+
+  res.send(garden);
 });
 
 app.get('/', function(req,res){
    // res.send("Welcome to Nathaniel's Plant Monitoring System");
-   // res.sendFile(__dirname + '/public/index.html');
-   res.status(200).render('/public/index.html')
-   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-   console.log('Status : ' + led.readSync() + ' - ip : ' + ip);
+   res.sendFile(__dirname + '/public/index.html');
+   // res.status(200).render('/public/index.html')
+   // var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 });
 
 // Change the status of LED via API based on parameters sent by Button
@@ -47,11 +55,26 @@ app.get('/led/:state', function (req, res) {
     res.send('Status ' + req.params.state);
 });
 
+app.get('/garden', function (req, res) {
+    res.send(garden);
+});
+
 // CREATE SERVER
 app.listen(port, function(){
  console.log('Server started, listening on port: ',port);
 });
 
+
+function containsObject(childObj, parentObj){
+  var o;
+  for(o in parentObj){
+    if(parentObj.hasOwnProperty(o) && parentObj[o] === childObj){
+      return true;
+    }
+  }
+
+  return false;
+}
 
 //http.createServer(function (req, res) {
 //  res.setHeader('Content-Type', 'text/html');
