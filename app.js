fe05897e077fc35035517c57243879e294d299da
Nathaniel Hobbs <hobbs.nathaniel@gmail.com>
2016-08-21 18:10:54 +0800

    added mac address to web page and editable name field

--- app.js
+++ app.js
@@ -6,12 +6,6 @@ var settings = require('./settings');
 var port = settings.port;
 var garden = {};
 
-
-// establish streams for RaspPi components
-// var GpioStream = require('gpio-stream');
-// var button = GpioStream.readable(17);
-// var led = GpioStream.writable(18);
-
 // set up logging middleware
 // var logger = require('morgan');
 // app.use(logger('common'));
@@ -21,13 +15,16 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({extended:false}));
 
+//avoid cross domain issues
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+    next();
+});
+
 // set view engine for rendering 
 app.set('view engine', 'ejs');
-
-// pipe button presses to led
-// var write_stream = button.pipe(led);
-// pipe button presses to stdout
-// button.pipe(process.stdout);
 
 // body should be of form {"name": xxx,"moisture": xxx}
 app.post('/garden', function(req,res) {
@@ -38,7 +35,7 @@ app.post('/garden', function(req,res) {
 
   //because working with objects, don't need to check for existance of plant in garden
   //if new then this will add to garden, otherwise will update whatever properties changed
-  var key = plant.name.split(' ', 1)[0]; //first word of plant for key
+  var key = plant.mac.replace(/:/g, ''); //use mac address (sub colons) as unique key
   garden[key] = plant;  
 
   console.log("garden is now ", JSON.stringify(garden))
@@ -51,11 +48,6 @@ app.get('/', function(req,res){
    res.sendFile(__dirname + '/public/index.html');
    // res.status(200).render('/public/index.html')
    // var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-});
-
-// Change the status of LED via API based on parameters sent by Button
-app.get('/led/:state', function (req, res) {
-    res.send('Status ' + req.params.state);
 });
 
 app.get('/garden', function (req, res) {
@@ -78,9 +70,3 @@ function containsObject(childObj, parentObj){
 
   return false;
 }
-
-//http.createServer(function (req, res) {
-//  res.setHeader('Content-Type', 'text/html');
-//  res.write('<pre>logging button presses:\n');
-//  write_stream.pipe(res);
-//}).listen(8080);
